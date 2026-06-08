79db59ed52dcf73bf9144812d7908a1bb50a0221
David Lima <davidlimma05@gmail.com>
2026-06-08 09:01:31 -0300

    Commit inicial, para criação do projeto

---
+++ src/main/java/com/david/academysystem/AcademysystemApplication.java
@@ -0,0 +1,13 @@
+package com.david.academysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademysystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/academysystem/AcademysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.academysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
