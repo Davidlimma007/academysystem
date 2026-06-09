20ebe40436498c17f890e2639a8e0b99509b3039
David Lima <davidlimma05@gmail.com>
2026-06-09 08:47:07 -0300

    Criação das entidades e seus relacionamentos

--- src/main/java/com/david/academysystem/database/model/Aluno.java
+++ src/main/java/com/david/academysystem/database/model/Aluno.java
@@ -1,4 +1,32 @@
 package com.david.academysystem.database.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "alunos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @OneToOne
+    @JoinColumn(name = "avaliacao_fisica_id")
+    private AvaliacoesFisicas avaliacoesFisicas;
+
+    @OneToMany(mappedBy = "aluno")
+    private Set<Treinos> treinos = new HashSet<>();
 }
--- src/main/java/com/david/academysystem/database/model/Exercicios.java
+++ src/main/java/com/david/academysystem/database/model/Exercicios.java
@@ -1,4 +1,27 @@
 package com.david.academysystem.database.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "exercicios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Exercicios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "grupo_muscular", nullable = false)
+    private String grupoMuscular;
+
 }
